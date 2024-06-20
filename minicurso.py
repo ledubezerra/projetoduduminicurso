c5ecf382dc331d883c99999a7ca20da305106767
ledubezerra <eduardo.bezerra202@gmail.com>
2024-06-20 16:15:13 -0300

    Update minicurso.py

--- minicurso.py
+++ minicurso.py
@@ -5,7 +5,7 @@ import streamlit as st
 def home():
 
     st.title("projeto pokemon")
-    st.write("Este é um projeto meu, dudu do bigode, que tem como objetivo mostrar algumas informações de extrema relevancia sobre pokemons através de gráficos.Você pode acessar esses gráficos atravez da barra de navegação ao lado.")
+    st.write("Este é um projeto meu, dudu do bigode, que tem como objetivo mostrar algumas informações de extrema relevancia sobre pokemons através de gráficos. Você pode acessar esses gráficos atravez da barra de navegação ao lado.")
 
 
 def carregar_dados():
